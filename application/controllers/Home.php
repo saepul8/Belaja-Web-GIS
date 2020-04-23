457bbdbe83efabaac785148ff63257b070a3d599
SAEPULOH <solusiweb12@gmail.com>
2020-04-23 15:53:36 +0700

    Add files via upload

---
+++ application/controllers/Home.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{ 
+    public function index()
+    {
+        $data=array(
+            'content'=>'map'
+        );
+        $this->load->view('template',$data);
+    }
+}
---
+++ application/views/map.php
@@ -0,0 +1,24 @@
+<div class="row">
+    <div class="col-md-4 shadow" style="padding:5px; border-radius:5px;">
+        <div class="card md-2">
+            <h4 class="card-header">Koordinat Kursor</h4>            
+            <div class="card-body">
+                <span id="coordinate"></span>
+            </div>
+        </div>
+        <div class="card mb-2" style="margin-top:10px;">
+            <h5 class="card-header">Informasi</h5>
+            <div class="card-body">
+
+            </div>
+        </div>
+    </div>
+    <div class="col-md-8">
+        <div class="card">
+            <div class="mapcanvas" id="map">
+
+            </div>   
+        </div>
+    </div>
+
+</div>
